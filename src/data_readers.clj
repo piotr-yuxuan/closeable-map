0092727063a168ba45bf70fb6cc615da6f8cf644
piotr-yuxuan <piotr-yuxuan@users.noreply.github.com>
2021-04-16 23:09:25 +0100

    Add data readers
    However, they do not overcome metadata limitations.

---
+++ src/data_readers.clj
@@ -0,0 +1,3 @@
+{closeable-map/swallow piotr-yuxuan.closeable-map/data-reader-swallow
+ closeable-map/ignore piotr-yuxuan.closeable-map/data-reader-ignore
+ closeable-map/fn piotr-yuxuan.closeable-map/data-reader-fn}
