76a15b817a24bd6f17e4e5cbdc6975f064ca6754
胡雨軒 Петр <piotr-yuxuan-github@2bst.fr>
2021-05-09 10:53:46 +0100

    Update project.clj

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject piotr-yuxuan/closeable-map (-> "./resources/closeable-map.version" slurp .trim)
-  :description "A Clojure map which implements java.io.Closeable"
+  :description "Application state management made simple: a Clojure map that implements java.io.Closeable."
   :url "https://github.com/piotr-yuxuan/closeable-map"
   :license {:name "European Union Public License 1.2 or later"
             :url "https://joinup.ec.europa.eu/collection/eupl/eupl-text-eupl-12"
