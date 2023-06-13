3ec62e1dba64aadc50e232b916932073293d5e37
Walter CI <piotr-yuxuan@users.noreply.github.com>
2023-06-13 06:34:40 +0000

    Update dependency versions

--- project.clj
+++ project.clj
@@ -19,7 +19,7 @@
              :dev {:global-vars {*warn-on-reflection* true}}
              :jar {:jvm-opts ["-Dclojure.compiler.disable-locals-clearing=false"
                               "-Dclojure.compiler.direct-linking=true"]}
-             :kaocha [:test {:dependencies [[lambdaisland/kaocha "1.84.1335"]]}]}
+             :kaocha [:test {:dependencies [[lambdaisland/kaocha "1.85.1342"]]}]}
   :deploy-repositories [["clojars" {:sign-releases false
                                     :url "https://clojars.org/repo"
                                     :username :env/WALTER_CLOJARS_USERNAME
