672d5f8f80655ba1a009b6cf66c84ccabbc5ee5d
Walter CI <piotr-yuxuan@users.noreply.github.com>
2024-05-01 06:35:27 +0000

    Update dependency versions

--- project.clj
+++ project.clj
@@ -15,7 +15,7 @@
                                       "state" "mount" "integrant" "closeable"
                                       "with-open" "clojure-maps" "system"]
                       :github/private? false}
-             :provided {:dependencies [[org.clojure/clojure "1.12.0-alpha10"]]}
+             :provided {:dependencies [[org.clojure/clojure "1.12.0-alpha11"]]}
              :dev {:global-vars {*warn-on-reflection* true}}
              :jar {:jvm-opts ["-Dclojure.compiler.disable-locals-clearing=false"
                               "-Dclojure.compiler.direct-linking=true"]}
